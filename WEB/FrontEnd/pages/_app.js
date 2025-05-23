62fc6ed494dfd52e19225c0fdd97e96c394f97b5
pyungjong <sonopj@gmail.com>
2025-05-23 19:16:43 +0900

    distribute WEB

--- WEB/FrontEnd/pages/_app.js
+++ WEB/FrontEnd/pages/_app.js
@@ -1,13 +1,12 @@
 import '../styles/globals.css';
-import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://223.194.45.67:8003', // ✅ 꼭 uri!
+  uri: '/api/graphql-proxy', // 서버리스 함수 통해 우회
   cache: new InMemoryCache(),
 });
 
 export default function App({ Component, pageProps }) {
-
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
