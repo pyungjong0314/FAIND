fefc7ecf867e49b047ff457517a9dc4b4e541533
wlsgudy3 <wlsgudy3@naver.com>
2025-05-25 14:20:02 +0900

    modifying ip address & adding library db post code

--- yolo_inner/app/library_processor.py
+++ yolo_inner/app/library_processor.py
@@ -6,6 +6,42 @@ from notifier import send_lost_to_web_server
 import json
 import os
 
+import requests
+from dotenv import load_dotenv
+
+url = "http://localhost:4000/graphql"  # 여기에 GraphQL 서버 주소를 입력
+headers = {"Content-Type": "application/json"}
+
+
+def post_item_to_main_server(timestamp, location, missing_items ):
+    for item in missing_items:
+        data = {
+            "query": """
+                mutation CreateLost($createLostInput: CreateLostInput) {
+                    createLost(createLostInput: $createLostInput)
+                }
+            """,
+            "variables": {
+                "createLostInput": {
+                    "lost_date": timestamp.isoformat(),
+                    "lost_location": location,
+                    "lost_name": item, 
+                    "person_id": 1, # 추후에 광운대 학생/외부인 구분 예정.
+                    "status": "found" # found가 분실물인 것.
+                }
+            }
+        }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        print(f"[NOTIFIER] Item sent for server → missing: {missing_items}")
+        return True
+    except requests.RequestException as e:
+        print(f"[NOTIFIER] Failed to send alert: {e}")
+        return False
+
+
 os.makedirs("images", exist_ok=True)
 os.makedirs("images/items", exist_ok=True)
 
@@ -192,6 +228,7 @@ while True:
                                 save_path = os.path.join("images/items", filename)
                                 cv2.imwrite(save_path, item_crop)
                                 print(f"[INFO] 분실물 이미지 저장 완료: {save_path}")
+                                post_item_to_main_server(datetime.now(), "도서관 내부", obj.class_name)
                             break
                         
         for tid, obj in tracked_objects.items():
