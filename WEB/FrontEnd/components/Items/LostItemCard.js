942c4a6a16fe54ce3d13277ca37dfa541c7b5add
pyungjong <sonopj@gmail.com>
2025-06-05 19:58:53 +0900

    fix: Image proxy

--- WEB/FrontEnd/components/Items/LostItemCard.js
+++ WEB/FrontEnd/components/Items/LostItemCard.js
@@ -2,10 +2,15 @@ import * as Styles from './ItemSectionStyle';
 
 export default function LostItemCard({ title, location, date, image }) {
   const isValidImage = image && image !== 'null' && image !== 'undefined';
+
+  // 프록시 URL 생성
+  const proxiedImage = isValidImage
+    ? `https://images.weserv.nl/?url=${encodeURIComponent(image)}`
+    : null;
   
   return (
     <Styles.Item>
-      <Styles.ItemImage image={isValidImage ? image : null} />
+      <Styles.ItemImage image={proxiedImage} />
       <Styles.ItemText>
         <div>
           <Styles.ItemTextTitle>{title}</Styles.ItemTextTitle>
